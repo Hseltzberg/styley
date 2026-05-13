112510dc1b99874c9c04494a3d980e5929efc624
Hayley <hseltzbe@chicagobooth.edu>
2026-05-13 04:13:15 +0000

    navbar with links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action(:authenticate_user!, { :only => [:home] })
+
   def home
     render({ :template => "page_templates/home" })
   end
+  
 end
