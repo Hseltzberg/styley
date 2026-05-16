719a579d36dce97c312c185b3ad1756b684436e3
Hayley <hseltzbe@chicagobooth.edu>
2026-05-16 19:06:23 +0000

    home page working!!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,16 @@ class PagesController < ApplicationController
   skip_before_action(:authenticate_user!, { :only => [:home] })
 
   def home
+    if current_user.present?
+      @list_of_public_outfits = Outfit.where({ :is_public => true }).where.not({ :user_id => current_user.id })
+    else
+      @list_of_public_outfits = []
+    end
+
     render({ :template => "page_templates/home" })
   end
 
   def about
-    render({ :template => "page_templates/about"})
+    render({ :template => "page_templates/about" })
   end
 end
