a3c485c540ef819fd6f98c5e6b536d904393e0ca
Hayley <hseltzbe@chicagobooth.edu>
2026-05-29 19:24:38 +0000

    last commit

--- app/uploaders/outfit_photo_uploader.rb
+++ app/uploaders/outfit_photo_uploader.rb
@@ -1,11 +1,13 @@
 class OutfitPhotoUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+end
+
   # Include RMagick, MiniMagick, or Vips support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   # include CarrierWave::Vips
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
