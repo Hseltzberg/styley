fef4603f2db042b5e283459cb730c155abd14735
Hayley <hseltzbe@chicagobooth.edu>
2026-06-01 20:33:28 +0000

    trying to fix render

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV.fetch("CLOUDINARY_CLOUD_NAME")
-  config.api_key = ENV.fetch("CLOUDINARY_API_KEY")
-  config.api_secret = ENV.fetch("CLOUDINARY_API_SECRET")
+  config.cloud_name = ENV.fetch("CLOUDINARY_CLOUD_NAME", nil)
+  config.api_key = ENV.fetch("CLOUDINARY_API_KEY", nil)
+  config.api_secret = ENV.fetch("CLOUDINARY_API_SECRET", nil)
   config.cdn_subdomain = true
 end
