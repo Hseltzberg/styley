a70922a9e77ca2f42fd43a2b5609cce3c04ce996
Hayley <hseltzbe@chicagobooth.edu>
2026-06-02 22:59:49 +0000

    pws

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: ENV.fetch("SITE_USERNAME", "styley"),
+    password: ENV.fetch("SITE_PASSWORD", "admin")
+  )
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, { :if => :devise_controller? }
 
